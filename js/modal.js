62a7c9aefaf992fae13f1c3e400e394c6a826b52
bykovaEkaterina1905 <luchikhinaea@gmail.com>
2024-03-06 10:12:53 +0400

    добавили плавное появление и исчезновение search

--- js/modal.js
+++ js/modal.js
@@ -3,9 +3,11 @@ const modalBtn = document.querySelector('.icon_search');
 const modalClose = modal.querySelector('.search-close-switch');
 
 modalBtn.addEventListener('click', () => {
-  modal.style.display = 'block';
+  //modal.style.display = 'block';
+  modal.classList.add('active');
 });
 
 modalClose.addEventListener('click', () => {
-  modal.style.display = 'none';
+  //modal.style.display = 'none';
+  modal.classList.remove('active');
 });
